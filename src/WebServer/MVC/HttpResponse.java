f3eb4f6f4f75afcca30d10b54a07a0afcf1dc3c1
Cole Green <17110935+nox7@users.noreply.github.com>
2021-11-27 15:45:09 -0600

    Create HttpResponse.java

---
+++ src/WebServer/MVC/HttpResponse.java
@@ -0,0 +1,11 @@
+package WebServer.MVC;
+
+import WebServer.Headers;
+
+public class HttpResponse {
+    public int statusCode;
+    public String body;
+    public Headers headers;
+
+    public HttpResponse(){}
+}
