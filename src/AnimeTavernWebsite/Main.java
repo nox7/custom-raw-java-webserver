4f61c429c2d72063c23b7eeb95b5f0b6592c5efd
Cole Green <17110935+nox7@users.noreply.github.com>
2021-11-27 15:58:17 -0600

    Test response from controller match

--- src/AnimeTavernWebsite/Main.java
+++ src/AnimeTavernWebsite/Main.java
@@ -3,34 +3,44 @@ package AnimeTavernWebsite;
 import AnimeTavernWebsite.Controllers.HomeController;
 import WebServer.MVC.Annotations.Route;
 import WebServer.MVC.BaseController;
+import WebServer.MVC.HttpResponse;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 
 public class Main {
 
-    public static ArrayList<BaseController> registeredControllers = new ArrayList<>(
-            new HomeController()
-    );
+    public static ArrayList<BaseController> registeredControllers = new ArrayList<>();
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
         // Register MVC controllers here
         registeredControllers.add(new HomeController());
 
+        String testURI = "/";
+
 
         // Ignore below
+        controllerLoop:
         for(BaseController c : Main.registeredControllers){
             Method[] instanceMethods = c.getClass().getMethods();
+            methodLoop:
             for (Method m : instanceMethods) {
                 Route[] methodRoutes = m.getDeclaredAnnotationsByType(Route.class);
                 if (methodRoutes.length > 0) {
                     for (Route r : methodRoutes) {
-                        System.out.println(r.uri());
+                        if (r.uri().equalsIgnoreCase(testURI)){
+                            try {
+                                HttpResponse response = (HttpResponse) m.invoke(c);
+                                System.out.println(response.body);
+                                break controllerLoop;
+                            }catch(IllegalAccessException e){
+
+                            }catch(InvocationTargetException e){
+
+                            }
+                        }
                     }
                 }
             }
