cb77b6a7afbe78db3b3c88103486b1bcfbdcecf8
Cole Green <17110935+nox7@users.noreply.github.com>
2021-12-05 15:48:26 -0600

    Update HomeController.java

--- src/AnimeTavernWebsite/Controllers/HomeController.java
+++ src/AnimeTavernWebsite/Controllers/HomeController.java
@@ -3,16 +3,27 @@ package AnimeTavernWebsite.Controllers;
 import WebServer.MVC.Annotations.Route;
 import WebServer.MVC.BaseController;
 import WebServer.MVC.HttpResponse;
+import WebServer.MVC.ViewParser.View;
+import WebServer.MVC.ViewParser.ViewParser;
 
 public class HomeController extends BaseController {
 
     @Route(method = "get", uri = "/")
     @Route(method = "get", uri = "/home")
     public HttpResponse homeView(){
+        System.out.println("INVOKED controller method");
+
+        ViewParser vp = new ViewParser();
+        View view = vp.getView("home.html");
+
+        System.out.println("EEEEE: " + view.body.toString());
+        System.out.println("== View Body ==: ".concat(view.body));
 
         HttpResponse response = new HttpResponse();
         response.statusCode = 200;
         response.body = "<p>Hello</p>";
+
+        System.out.println("Responsive parsed from controller");
 
         return response;
     }
